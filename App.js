1663eac87e2e36891eedae496abc1f57e58f0c5d
Abdul Afeel <abdulafeel2@gmail.com>
2022-09-25 20:11:37 +0530

    Text Input Added

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
-import React from 'react';
-
+import React, {useState, useCallback} from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -10,16 +9,35 @@ import {
   View,
   Image,
   ImageBackground,
+  TextInput,
 } from 'react-native';
+import axios from 'axios';
 
 const App = () => {
+  const [input, setInput] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+
+  const fetchDataHandler = useCallback(() => {}, []);
+
   return (
     <View style={styles.container}>
       <StatusBar translucent backgroundColor="transparent" />
       <ImageBackground
         source={require('./assets/images/weatherbg.jpg')}
         resizeMode="cover"
-        style={styles.image}></ImageBackground>
+        style={styles.image}>
+        <View>
+          <TextInput
+            placeholder="Enter city name and press return..."
+            onChangeText={text => setInput(text)}
+            value={input}
+            placeholderTextColor={'#000'}
+            style={styles.textinput}
+            onSubmitEditing={fetchDataHandler}
+          />
+        </View>
+      </ImageBackground>
     </View>
   );
 };
@@ -28,10 +46,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-
   image: {
     flex: 1,
     flexDirection: 'column',
+  },
+  textinput: {
+    borderBottomWidth: 3,
+    padding: 5,
+    paddingVertical: 20,
+    marginVertical: 100,
+    marginHorizontal: 10,
+    backgroundColor: '#fff',
+    fontSize: 19,
+    borderRadius: 16,
+    borderBottomColor: '#df8e00',
   },
 });
 
