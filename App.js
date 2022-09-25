4c5a14638e232e06b8220051836d672038dd6fca
Abdul Afeel <abdulafeel2@gmail.com>
2022-09-25 21:04:29 +0530

    Loading added

--- App.js
+++ App.js
@@ -10,6 +10,9 @@ import {
   Image,
   ImageBackground,
   TextInput,
+  KeyboardAvoidingView,
+  Dimensions,
+  ActivityIndicator,
 } from 'react-native';
 import axios from 'axios';
 
@@ -18,10 +21,29 @@ const App = () => {
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState([]);
 
-  const fetchDataHandler = useCallback(() => {}, []);
+  const api = {
+    baseUrl: '//api.openweathermap.org/data/2.5/',
+    key: 'c4bce77e4fa711168cfec2e9fea09ca2',
+  };
+
+  const fetchDataHandler = useCallback(() => {
+    setLoading(true);
+    setInput('');
+    axios({
+      method: 'GET',
+      url: `https://api.openweathermap.org/data/2.5/weather?q=${input}&units=metrics&appid=${api.key}`,
+    })
+      .then(res => {
+        console.log(res.data);
+        setData(res.data);
+      })
+      .catch(e => console.dir(e))
+      .finally(() => setLoading(false));
+  }, [api.key, input]);
 
   return (
     <View style={styles.container}>
+      <KeyboardAvoidingView behavior="position" />
       <StatusBar translucent backgroundColor="transparent" />
       <ImageBackground
         source={require('./assets/images/weatherbg.jpg')}
@@ -32,11 +54,17 @@ const App = () => {
             placeholder="Enter city name and press return..."
             onChangeText={text => setInput(text)}
             value={input}
-            placeholderTextColor={'#000'}
+            placeholderTextColor="#D3D3D3"
             style={styles.textinput}
             onSubmitEditing={fetchDataHandler}
           />
         </View>
+        {loading && (
+          <View>
+            <ActivityIndicator size={'large'} color="#000" />
+          </View>
+        )}
+        {data && <View style={styles.infoview}></View>}
       </ImageBackground>
     </View>
   );
@@ -60,6 +88,7 @@ const styles = StyleSheet.create({
     fontSize: 19,
     borderRadius: 16,
     borderBottomColor: '#df8e00',
+    color: 'black',
   },
 });
 
